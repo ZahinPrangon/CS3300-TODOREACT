f44fbecadf42af1f9f73db9da5b27c29ef6c8d08
Zahin430 <zhprangon@mun.ca>
2019-12-02 11:42:40 -0330

    updated

--- src/components/Todo-Data/Todo-Data.js
+++ src/components/Todo-Data/Todo-Data.js
@@ -3,65 +3,65 @@ const todosData = [
     id: 1,
     title: "Add Edit for todo title",
     completed: false,
-    start: new Date("December 1 2019 03:24:00"),
-    end: new Date("December 1 2019 05:24:00"),
+    start: new Date(),
+    end: new Date(),
     type: "personal",
     rating: 4
   },
   {
     id: 2,
     title: "Talk to Jian Tang after class at 1pm",
-    start: new Date("December 1 2019 07:24:00"),
-    end: new Date("December 1 2019 08:24:00"),
+    start: new Date(),
+    end: new Date(),
     completed: false,
     type: "personal",
     rating: 2
+  },
+  {
+    id: 3,
+    title: "Finish CS3401",
+    start: new Date("November 29 2019 03:24:00"),
+    end: new Date("November 30 2019 04:24:00"),
+    completed: false,
+    type: "work",
+    rating: 5
+  },
+  {
+    id: 4,
+    title: "Go to work at 11am",
+    completed: false,
+    start: new Date("November 29 2019 03:24:00"),
+    end: new Date("November 30 2019 04:24:00"),
+    type: "personal",
+    rating: 1
+  },
+  {
+    id: 5,
+    title: "Exam ends",
+    completed: false,
+    start: new Date("November 30 2019 09:00:00"),
+    end: new Date("December 11 2019 11:00:00"),
+    type: "personal",
+    rating: 5
+  },
+  {
+    id: 6,
+    title: "Exam Starts",
+    completed: false,
+    start: new Date("December 5 2019 09:00:00"),
+    end: new Date("December 5 2019 11:00:00"),
+    type: "personal",
+    rating: 2
+  },
+  {
+    id: 7,
+    title: "Exam donne",
+    completed: false,
+    start: new Date("November 29 2019 03:24:00"),
+    end: new Date("November 30 2019 04:24:00"),
+    type: "personal",
+    rating: 2
   }
-  // {
-  //   id: 3,
-  //   title: "Finish CS3401",
-  //   start: new Date("November 29 2019 03:24:00"),
-  //   end: new Date("November 30 2019 04:24:00"),
-  //   completed: false,
-  //   type: "work",
-  //   rating: 5
-  // },
-  // {
-  //   id: 4,
-  //   title: "Go to work at 11am",
-  //   completed: false,
-  //   start: new Date("November 29 2019 03:24:00"),
-  //   end: new Date("November 30 2019 04:24:00"),
-  //   type: "personal",
-  //   rating: 1
-  // },
-  // {
-  //   id: 5,
-  //   title: "Exam ends",
-  //   completed: false,
-  //   start: new Date("November 30 2019 09:00:00"),
-  //   end: new Date("December 11 2019 11:00:00"),
-  //   type: "personal",
-  //   rating: 5
-  // },
-  // {
-  //   id: 6,
-  //   title: "Exam Starts",
-  //   completed: false,
-  //   start: new Date("December 5 2019 09:00:00"),
-  //   end: new Date("December 5 2019 11:00:00"),
-  //   type: "personal",
-  //   rating: 2
-  // },
-  // {
-  //   id: 7,
-  //   title: "Exam donne",
-  //   completed: false,
-  //   start: new Date("November 29 2019 03:24:00"),
-  //   end: new Date("November 30 2019 04:24:00"),
-  //   type: "personal",
-  //   rating: 2
-  // }
 ];
 
 export default todosData;
